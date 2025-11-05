41798b35f3a496928fa2a76cbe20a939e915d528
MonroeLionel <MonroeLionel@gmail.com>
2025-11-05 22:06:43 +0300

    feat: add queue solution

--- src/queue.js
+++ src/queue.js
@@ -12,20 +12,61 @@ const { NotImplementedError } = require('../lib/errors');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
 class Queue {
+  constructor() {
+    this.items = []
+  }
   getUnderlyingList() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+
+    let tempResult
+    if (this.value == null) {
+      return null
+    } else {
+      tempResult = { value: this.value, next: this.next }
+    }
+    let tempList = this
+    let temp = tempResult
+    while (tempList.next) {
+      tempList = tempList.next
+      temp.next = { value: tempList.value, next: tempList.next }
+      temp = temp.next
+    }
+    return tempResult
   }
 
-  enqueue(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    const newNode = new ListNode(value)
+
+    if (!this.value) {
+      this.value = value
+      this.next = null
+    } else {
+      let tempList = this
+      while (tempList.next) {
+        tempList = tempList.next
+      }
+      tempList.next = newNode
+    }
   }
 
   dequeue() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this.value) {
+      return undefined
+    }
+    const temp = this.value
+    this.value = this.next.value
+    let temp2 = this.next
+    this.next = null
+    this.next = temp2.next
+    return temp
   }
 }
 
